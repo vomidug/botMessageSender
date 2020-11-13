2f4368fc4bc563430a32cfdfe0b67ff8a679c87f
vomidug <vomidug@gmail.com>
2020-11-13 17:10:18 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,94 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+	<v-app>
+		<v-main>
+		<v-container>
+			<v-container>
+				<v-container>
+
+						<!--
+							<v-textarea
+								solo
+								name="input-7-1"
+								label="Default style"
+								value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
+								hint="Enter adds new line, not sends"
+								v-model="message"
+							></v-textarea>
+						-->
+
+					<v-text-field
+						label="Type your message"
+						v-model="message"
+					>
+					</v-text-field>
+
+
+				</v-container>
+
+				<v-container>
+					<v-btn @click='send("chat", $event)'>
+						Send to Chatik
+					</v-btn>
+				</v-container>
+				<v-container>
+					<v-btn @click='send("channel", $event)'>
+						Send to Notifications Channel
+					</v-btn>
+				</v-container>
+				<v-container>
+					<v-btn @click='send("vomidug",$event) '>
+						Send to vomidug
+					</v-btn>
+				</v-container>
+			</v-container>
+		</v-container>
+		</v-main>
+	</v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import axios from 'axios' 
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
+	name: "App",
+
+	data: () => ({
+		message:"",
+	}),
+
+	methods: {
+		async send(dest){
+
+			console.log(dest)
+
+			var msg = this.message;
+			var res = {}
+			if(msg){
+				if(dest == 'vomidug' || dest == 'chat'){
+					res = await axios.post('http://hm.vomidug.xyz/hm/bot/api/'+dest, {msg})
+					if (res.status === 200){
+						this.$toast("Отправлено!")
+					} else {
+						this.$toast("Не удалось отправить, возможно, апишка недоступна(")
+					}
+				} else if (dest == 'channel'){
+					res = await axios.post('http://hm.vomidug.xyz/hm/bot/api/'+dest, {msg})
+					if (res.status === 200){
+						this.$toast("Отправлено!")
+					} else {
+						this.$toast("Не удалось отправить, возможно, апишка недоступна(")
+					}
+
+				}
+				console.log(msg)
+			} else(
+				this.$toast('Не надо отправлять пустое сообщение, пожалуйста')
+			)
+
+			axios.get()
+
+		}
+	}
+
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
+Vue.use(Toast);
 
 Vue.config.productionTip = false;
 
 new Vue({
+  vuetify,
+
   render: function(h) {
     return h(App);
   }
